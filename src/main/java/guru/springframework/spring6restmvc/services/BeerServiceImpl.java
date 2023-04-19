1e48b6d8c9d09d5341badc4b85d48fe7c544d319
Simon Oser <simon.oser@gmail.com>
2023-04-19 18:55:45 +0200

    55. Project Lombok Logging

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,19 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID uuid) {
+        log.debug("BeerServiceImpl.getBeerById was called with uuid:" + uuid);
         return Beer.builder()
                 .id(uuid)
                 .version(1)

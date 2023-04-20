0e5e08d820dd8701cbc0627d6fd0d12a0e96dfdf
Simon Oser <simon.oser@gmail.com>
2023-04-20 17:33:36 +0200

    58. Introducing SFG Beer Works

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,15 +4,22 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @GetMapping("/api/v1/beer")
+    public List listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID uuid) {
         log.debug("BeerController.getBeerById(uuid:" + uuid + ")");
--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
     Beer getBeerById(UUID uuid);
 }
